ea0e3a5245ac10e14f2d471072ccabc8a2afed20
CHUNG WEI YONG <chungwyong@gmail.com>
2026-09-06 20:56:47 +0800

    feat: persist sqlite/chroma in volume and setup rag signals

--- agent/signals.py
+++ agent/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from blog.models import Post
 from portfolio.models import Project
-from services import KnowLedgeBaseService
+from .services import KnowLedgeBaseService
 
 kb_service=KnowLedgeBaseService()
 
